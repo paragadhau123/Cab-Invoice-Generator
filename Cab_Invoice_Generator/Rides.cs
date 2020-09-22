86bba3fcee7d9b49725835cb8518e6853d35756b
Parag Adhau <adhauparag64@gmail.com>
2020-09-22 23:31:46 +0530

    Added [UC 2] :Ability when multiple rides are given then should return total fare

---
+++ Cab_Invoice_Generator/Rides.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cab_Invoice_Generator
+{
+    public class Rides
+    {
+        public double distance;
+        public int time;
+
+        public Rides(double inputDistance, int inputTime)
+        {
+            distance = inputDistance;
+            time = inputTime;
+        }
+    }
+}
