97e78e28d90707c6116e928fe46e7a8d7de2b849
Parag Adhau <adhauparag64@gmail.com>
2020-09-22 22:46:55 +0530

    Added[TC 1.1] :To check total fare when distance and time is given

--- Cab_Invoice_Generator_Test/TestMethod.cs
+++ Cab_Invoice_Generator_Test/TestMethod.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-
+using Cab_Invoice_Generator;
 namespace Cab_Invoice_Generator_Test
 {
     public class Tests
@@ -13,6 +13,11 @@ namespace Cab_Invoice_Generator_Test
         public void GivenDistanceaAndTime_ShouldReturnTotalFare()
         {
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
         }
     }
 }
