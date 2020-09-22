8acda50aa05d20bb821d7c93cd92bc0a355bae62
Parag Adhau <adhauparag64@gmail.com>
2020-09-22 19:30:05 +0530

    Added :The welcome message

---
+++ Cab_Invoice_Generator_Test/TestMethod.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Cab_Invoice_Generator_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
